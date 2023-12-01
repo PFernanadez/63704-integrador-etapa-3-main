f6048d5a074355afc988250d42aee7292efd794a
PFernanadez <pedro.nacho997@gmail.com>
2023-12-01 17:52:47 -0300

    udate 3

--- back/config/conectarMongo.js
+++ back/config/conectarMongo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose'
 
 const conectarMongo = async () => {
@@ -11,18 +10,4 @@ const conectarMongo = async () => {
         console.log('Error al conectar a Mongo DB', error)
     }
 }
-=======
-import mongoose from 'mongoose'
-
-const conectarMongo = async () => {
-
-    try {
-        await mongoose.connect(process.env.URI_REMOTA)
-        //await mongoose.connect(process.env.URI_REMOTA)
-        console.log('Conexión a Mongo realizada con éxito!')
-    } catch (error) {
-        console.log('Error al conectar a Mongo DB', error)
-    }
-}
->>>>>>> d207792c8bd3057dfe29902addbdd504c3322ce1
 export default conectarMongo
